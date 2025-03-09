16936c47ea3052cb4de50cda74a8efd829dbfa6c
chrischriscris <18-10892@usb.ve>
2025-03-09 12:19:32 -0400

    readded copilot, it's time

--- .config/nvim/lua/chus/plugins.lua
+++ .config/nvim/lua/chus/plugins.lua
@@ -74,7 +74,21 @@ local plugins = {
 	{ "hrsh7th/cmp-nvim-lsp" },
 	{ "hrsh7th/nvim-cmp" },
 	{ "L3MON4D3/LuaSnip" },
-	-- { "github/copilot.vim" },
+	{ "github/copilot.vim" },
+    {
+      {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+          { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+          { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        },
+        build = "make tiktoken", -- Only on MacOS or Linux
+        opts = {
+          -- See Configuration section for options
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+      },
+    },
 	{ "eandrju/cellular-automaton.nvim" },
 	{ "laytan/cloak.nvim" },
 	{ "kaarmu/typst.vim", ft = "typst", lazy = false },
