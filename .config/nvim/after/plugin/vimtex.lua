ae4c0debd2802c740bd7d00cc90203cca885d8bc
chrischriscris <18-10892@usb.ve>
2025-03-27 00:53:11 -0400

    delete vimtex configs

--- .config/nvim/after/plugin/vimtex.lua
+++
@@ -1,9 +0,0 @@
--- Set remap to view the pdf file of the current tex file in the default pdf viewer
-
-vim.g.vimtex_view_method = 'zathura'
-
--- Set remap to compile the current tex file
--- vim.api.nvim_set_keymap('n', '<leader>vc', ':VimtexCompile<CR>', { noremap = true, silent = true })
-
--- Set remap to view the pdf file of the current tex file
-vim.api.nvim_set_keymap('n', '<leader>vp', ':VimtexView<CR>', { noremap = true, silent = true })
