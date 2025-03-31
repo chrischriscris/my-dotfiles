c75630d48907a729c26df04a866fc2351ab362e2
chrischriscris <18-10892@usb.ve>
2025-03-31 07:46:07 -0400

    updated ai plugins

--- .config/nvim/lua/chus/plugins.lua
+++ .config/nvim/lua/chus/plugins.lua
@@ -82,11 +82,102 @@ local plugins = {
     { "neovim/nvim-lspconfig" },
     { "hrsh7th/cmp-nvim-lsp" },
     { "hrsh7th/nvim-cmp" },
+
+    -- TODO: CONFIGURE LATER
+    -- {
+    --     "saghen/blink.cmp",
+    --     -- optional: provides snippets for the snippet source
+    --     dependencies = { "rafamadriz/friendly-snippets" },
+
+    --     -- use a release tag to download pre-built binaries
+    --     version = "1.*",
+    --     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    --     -- build = 'cargo build --release',
+    --     -- If you use nix, you can build from source using latest nightly rust with:
+    --     -- build = 'nix run .#build-plugin',
+
+    --     ---@module 'blink.cmp'
+    --     ---@type blink.cmp.Config
+    --     opts = {
+    --         -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    --         -- 'super-tab' for mappings similar to vscode (tab to accept)
+    --         -- 'enter' for enter to accept
+    --         -- 'none' for no mappings
+    --         --
+    --         -- All presets have the following mappings:
+    --         -- C-space: Open menu or open docs if already open
+    --         -- C-n/C-p or Up/Down: Select next/previous item
+    --         -- C-e: Hide menu
+    --         -- C-k: Toggle signature help (if signature.enabled = true)
+    --         --
+    --         -- See :h blink-cmp-config-keymap for defining your own keymap
+    --         keymap = { preset = "default" },
+
+    --         appearance = {
+    --             -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    --             -- Adjusts spacing to ensure icons are aligned
+    --             nerd_font_variant = "mono",
+    --         },
+
+    --         -- (Default) Only show the documentation popup when manually triggered
+    --         completion = { documentation = { auto_show = false } },
+
+    --         -- Default list of enabled providers defined so that you can extend it
+    --         -- elsewhere in your config, without redefining it, due to `opts_extend`
+    --         sources = {
+    --             default = { "lsp", "path", "snippets", "buffer" },
+    --         },
+
+    --         -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    --         -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    --         -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --         --
+    --         -- See the fuzzy documentation for more information
+    --         fuzzy = { implementation = "prefer_rust_with_warning" },
+    --     },
+    --     opts_extend = { "sources.default" },
+    -- },
+    -- {
+    --     'neovim/nvim-lspconfig',
+    --     dependencies = { 'saghen/blink.cmp' },
+
+    --     -- example using `opts` for defining servers
+    --     opts = {
+    --         servers = {
+    --             lua_ls = {}
+    --         }
+    --     },
+
+    --     -- example calling setup directly for each LSP
+    --     config = function()
+    --         local capabilities = require('blink.cmp').get_lsp_capabilities()
+    --         local lspconfig = require('lspconfig')
+
+    --         lspconfig['lua_ls'].setup({ capabilities = capabilities })
+    --     end
+    -- },
+
     { "L3MON4D3/LuaSnip" },
-    -- { "github/copilot.vim" },
+
+    -- AI assistants
+    {
+        "supermaven-inc/supermaven-nvim",
+        config = function()
+            require("supermaven-nvim").setup({})
+        end,
+    },
+    {
+        "olimorris/codecompanion.nvim",
+        config = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+    },
+
     { "eandrju/cellular-automaton.nvim" },
     { "laytan/cloak.nvim" },
-    { "kaarmu/typst.vim",                 ft = "typst",   lazy = false },
+    { "kaarmu/typst.vim",               ft = "typst", lazy = false },
 
     -- These optional plugins should be loaded directly because of a bug in Packer lazy loading
     { "nvim-tree/nvim-web-devicons" },
