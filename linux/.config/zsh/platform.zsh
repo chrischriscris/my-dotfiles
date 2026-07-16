bd1efd83529abcddb5226b96ea51c4b5dd3ff3c8
chrischriscris <ruckrollhuke@gmail.com>
2026-07-16 15:23:59 -0400

    remove fzf shi

--- linux/.config/zsh/platform.zsh
+++ linux/.config/zsh/platform.zsh
@@ -1,6 +1,3 @@
-[[ -r /usr/share/fzf/key-bindings.zsh ]] && source /usr/share/fzf/key-bindings.zsh
-[[ -r /usr/share/fzf/completion.zsh ]] && source /usr/share/fzf/completion.zsh
-
 TIMEFMT='%J   %U  user %S system %P cpu %*E total'$'\n'\
 'avg shared (code):         %X KB'$'\n'\
 'avg unshared (data/stack): %D KB'$'\n'\
--- macos/.config/zsh/platform.zsh
+++ macos/.config/zsh/platform.zsh
@@ -1,9 +1,3 @@
-# fzf installed by Homebrew.
-source /opt/homebrew/opt/fzf/shell/key-bindings.zsh 2>/dev/null || \
-    source /usr/local/opt/fzf/shell/key-bindings.zsh 2>/dev/null
-source /opt/homebrew/opt/fzf/shell/completion.zsh 2>/dev/null || \
-    source /usr/local/opt/fzf/shell/completion.zsh 2>/dev/null
-
 TIMEFMT='%J   %U  user %S system %P cpu %*E total'$'\n'\
 'avg shared (code):         %X KB'$'\n'\
 'avg unshared (data/stack): %D KB'$'\n'\
